e14bf0792065449cca33b8d657d1e6c7ec21cec0
yotsuuuuuu <catstachex@gmail.com>
2026-01-27 15:48:07 -0500

    fixed ?!

--- ComponentFramework/World.cpp
+++ ComponentFramework/World.cpp
@@ -8,20 +8,20 @@ void World::createWorld(const TerrainPreset& preset)
 
 void World::update(Vec3 playerPosition)
 {
-	// determine which chunks to load/generate based on player position
-	Vec2 centerChunkCoord = {int(std::floor(playerPosition.x / CHUNK_WORLD_SIZE)),
-							  int(std::floor(playerPosition.z / CHUNK_WORLD_SIZE)) };
+	//// determine which chunks to load/generate based on player position
+	//Vec2 centerChunkCoord = {int(std::floor(playerPosition.x / CHUNK_WORLD_SIZE)),
+	//						  int(std::floor(playerPosition.z / CHUNK_WORLD_SIZE)) };
 
-	constexpr int RENDER_DISTANCE = 4; // number of chunks to load in each direction
+	//constexpr int RENDER_DISTANCE = 4; // number of chunks to load in each direction
 
-	for (int z = -RENDER_DISTANCE; z <= RENDER_DISTANCE; z++) {
-		for (int x = -RENDER_DISTANCE; x <= RENDER_DISTANCE; x++) {
-			Vec2 chunkCoord = centerChunkCoord + Vec2(float(x), float(z));
-			
-			// logic for loading/generating chunks
+	//for (int z = -RENDER_DISTANCE; z <= RENDER_DISTANCE; z++) {
+	//	for (int x = -RENDER_DISTANCE; x <= RENDER_DISTANCE; x++) {
+	//		Vec2 chunkCoord = centerChunkCoord + Vec2(float(x), float(z));
+	//		
+	//		// logic for loading/generating chunks
 
-		}
-	}
+	//	}
+	//}
 }
 
 void World::renderWorld(VkCommandBuffer commandBuffer)
